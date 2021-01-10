e874fdbcbfb06ceb774e6172a30b38871a0fe445
Oliver Payne <olliepayne@me.com>
2021-01-09 20:00:10 -0700

    virtual board matrix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+let boardMatrix = []
+const rows = 6
+const columns = 7
+
+function Cell() {
+  return (
+    <div>
+      <p>cell</p>
+    </div>
+  )
+}
+
+class Game extends React.Component {
+  componentDidMount() {
+    generateBoardMatrix()
+    console.log(boardMatrix)
+  }
+
+  render() {
+    return (
+      <div id="game">
+        
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
-);
+)
+
+function generateBoardMatrix() {
+  for(let y = 0; y < rows; y++) {
+    let newRow = []
+    for(let x = 0; x < columns; x++) {
+      newRow.push(null)
+    }
+    boardMatrix.push(newRow)
+  }
+}
